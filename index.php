3e3c91b23dd80460862013b08cdecd0022360021
Philip Tsang <philiptsang@v1020-wn-6-238.campus-dynamic.uwaterloo.ca>
2015-09-20 06:02:28 -0400

    hihiadidhdasdad

--- index.php
+++ index.php
@@ -10,27 +10,27 @@ require_once 'autoload.php';
     <title>Item Tracker</title>
     <!-- STYLES -->
     <!-- build:css lib/css/main.min.css -->
-    <link rel="stylesheet" type="text/css" href="/hackthenorth/bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/hackthenorth/bower_components/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="/hackthenorth/bower_components/rdash-ui/dist/css/rdash.min.css">
-    <link rel="stylesheet" type="text/css" href="/hackthenorth/css/index.css">
+    <link rel="stylesheet" type="text/css" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/bower_components/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/bower_components/rdash-ui/dist/css/rdash.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
     <!-- endbuild -->
     <!-- SCRIPTS -->
     <!-- build:js lib/js/main.min.js -->
-    <script type="text/javascript" src="/hackthenorth/bower_components/angular/angular.min.js"></script>
-    <script type="text/javascript" src="/hackthenorth/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
-    <script type="text/javascript" src="/hackthenorth/bower_components/angular-cookies/angular-cookies.min.js"></script>
-    <script type="text/javascript" src="/hackthenorth/bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
+    <script type="text/javascript" src="/bower_components/angular/angular.min.js"></script>
+    <script type="text/javascript" src="/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js"></script>
+    <script type="text/javascript" src="/bower_components/angular-cookies/angular-cookies.min.js"></script>
+    <script type="text/javascript" src="/bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
     <!-- endbuild -->
-    <link rel="stylesheet" href="/hackthenorth/bower_components/fullcalendar/dist/fullcalendar.css"/>
+    <link rel="stylesheet" href="/bower_components/fullcalendar/dist/fullcalendar.css"/>
     <!-- Custom Fonts -->
-    <link href="/hackthenorth/css/font-awesome-4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="/css/font-awesome-4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- jQuery -->
-    <script src="/hackthenorth/bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="/hackthenorth/bower_components/ngDialog/js/ngDialog.js"></script>
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="/bower_components/ngDialog/js/ngDialog.js"></script>
     <!-- Custom Scripts -->
-    <script type="text/javascript" src="/hackthenorth/angular/index.js"></script>
-<!--     <link rel="stylesheet" type="text/css" href="/hackthenorth/css/mod_style.css" /> -->
+    <script type="text/javascript" src="/angular/index.js"></script>
+<!--     <link rel="stylesheet" type="text/css" href="/css/mod_style.css" /> -->
 </head>
 <body ng-controller="controller">
     <div id="page-wrapper" ng-class="{'open': toggle}" ng-cloak>
